a1eeb21c982ca5d98579517e3afe6a3d1b8a4e78
Egor Pozhitok <egor_pozhitok@epam.com>
2023-02-20 17:33:00 +0100

    feat: fix environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default ({ mode }: { mode: 'production' | 'development' }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  return defineConfig({
-    plugins: [react(), eslintPlugin(), tsconfigPaths()],
-    base: '/fe-test/',
-    define: {
-      'process.env': process.env,
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react(), eslintPlugin(), tsconfigPaths()],
+  base: '/fe-test/',
+});
